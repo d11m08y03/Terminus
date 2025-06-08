905a9e88c00b371b5e8e2295b8dc5233357ae870
d11m08y03 <zakariyyakurmally@gmail.com>
2025-06-08 22:04:55 +0400

    Fix docker

--- Dockerfile
+++ Dockerfile
@@ -6,11 +6,4 @@ COPY . /app
 
 RUN pip install --no-cache-dir -r requirements.txt
 
-ENV DISCORD_BOT_TOKEN=""
-ENV VERIFICATION_CHANNEL_ID=""
-ENV VERIFIED_ROLE_ID=""
-ENV SENDER_EMAIL=""
-ENV SENDER_EMAIL_PASSWORD=""
-ENV EMAIL_DOMAIN=""
-
 CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import discord
 from bot.config import DISCORD_BOT_TOKEN
 from bot.client import Client
 from bot.database import init_db
-from bot.logging import init_logging, log_info
+from bot.logging import log_info
 
 init_db()
-init_logging()
 
 intents = discord.Intents.default()
 intents.message_content = True
